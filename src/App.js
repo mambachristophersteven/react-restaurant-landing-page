b2c12619894293f0695cff14ed59e62fd830e5d9
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-16 19:39:18 +0000

    adding createBrowserRouter and RouterProvider to App component to allow routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar />
+  }
+])
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <RouterProvider router={router} />
     </div>
   );
 }
