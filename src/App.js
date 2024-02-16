3fc467eed31c04206603f483a6fbff286a30dea7
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-16 19:54:45 +0000

    adding Outlet from react router dom so all my pages have Navbar and Footer once and for all

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
+
+const Layout = () => {
+  return(
+    <div className='app'>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />
-  },
-  {
-    path: "/about",
-    element: <About />
+    element: <Layout />,
+    children:[
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/about",
+        element: <About />
+      }
+    ]
   }
+  
 ])
 
 function App() {
