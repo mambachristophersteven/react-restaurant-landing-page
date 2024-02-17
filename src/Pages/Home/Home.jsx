0bec2530a9fee3546aadcfcb7698f640d9d16094
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-17 14:23:48 +0000

    initialized Button component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Home.css'
 import HomeBanner from '../../Components/HomeBanner/HomeBanner'
+import Button from '../../Components/Button/Button'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <div className="left">
             <p className="home-heading">Your Favourite Food Delivered Hot & Fresh</p>
             <p className="home-text">Healthy switcher chefs do all the prep work, like  peeding, chopping & marinating, so you can cook a fresh food.</p>
+            <Button />
         </div>
     </div>
   )
