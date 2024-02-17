72f75fa6c2a23d932edea23911ba8c9ff35f8cdb
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-17 14:33:46 +0000

    Button component styled

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import './Button.css'
+import arrow from '../../Assets/arrow.svg'
 const Button = () => {
   return (
-    <div>Button</div>
+    <div className='button'>
+        <button>
+            <p>order now</p>
+            <img src={arrow} alt="" />
+        </button>
+    </div>
   )
 }
 
