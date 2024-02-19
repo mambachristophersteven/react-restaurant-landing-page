32d7dcf07100d3958c89973c8c7a88bb8624bc3a
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-19 10:27:53 +0000

    content finished for AboutSection

--- src/Sections/AboutSection/AboutSection.jsx
+++ src/Sections/AboutSection/AboutSection.jsx
@@ -3,7 +3,8 @@ import './AboutSection.css'
 import AboutBanner from '../../Components/AboutBanner/AboutBanner'
 import about_image from '../../Assets/about-page-image.png'
 import arrow from '../../Assets/arrow.svg'
-
+import play_image from '../../Assets/play-circle.svg'
+import Button from '../../Components/Button/Button'
 
 const AboutSection = () => {
   return (
@@ -16,11 +17,14 @@ const AboutSection = () => {
             <p className="section-heading">about</p>
             <p className="section-header">Food Is An Important Part Of A Balanced Diet</p>
             <p className="section-text">Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et elit. Dolor  turpis molestie dui magnis facilisis at fringilla quam. <br />Non tincidunt magna non et elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.</p>
+            
             <div className="buttons">
-            <button>
-                <p>learn more</p>
-                <img src={arrow} alt="" />
-            </button>
+                <Button content= 'learn more' image = {arrow}/>
+                <div className="play">
+                    <img src={play_image} alt="" />
+                    <p className="play-text">watch video</p>
+                </div>
+
             </div>
         </div>
     </div>
