1c8f7402e0571c100691b2bfc457a43ea38af24e
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-19 11:00:12 +0000

    top section of WorkSection finished

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Home.css'
 import HomeSection from '../../Sections/HomeSection/HomeSection'
 import AboutSection from '../../Sections/AboutSection/AboutSection'
+import WorkSection from '../../Sections/WorkSection/WorkSection'
 
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
     <div className='home'>
       <HomeSection />
       <AboutSection />
+      <WorkSection />
     </div>
   )
 }
---
+++ src/Sections/WorkSection/WorkSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './WorkSection.css'
+const WorkSection = () => {
+  return (
+    <div className='work-section'>
+        <div className="top">
+            <p className="section-heading">work</p>
+            <p className="section-header">how it works</p>
+            <p className="section-text">Lorem ipsum dolor sit amet consectetur. Non tincidunt <br />magna non et elit. Dolor  turpis molestie dui <br />magnis facilisis at fringilla quam.</p>
+        </div>
+    </div>
+  )
+}
+
+export default WorkSection
