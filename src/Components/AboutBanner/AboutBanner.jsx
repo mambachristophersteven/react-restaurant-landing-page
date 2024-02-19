1796c53417eb759bd180efa8d9140d115bf1e0d2
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-19 09:27:08 +0000

    finished styling for AboutBanner

--- src/Components/AboutBanner/AboutBanner.jsx
+++ src/Components/AboutBanner/AboutBanner.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import './AboutBanner.css'
+import about_banner from '../../Assets/about-banner.svg'
 const AboutBanner = () => {
   return (
-    <div>AboutBanner</div>
+    <div className='about-banner'>
+        <img src={about_banner} alt="" />
+    </div>
   )
 }
 
