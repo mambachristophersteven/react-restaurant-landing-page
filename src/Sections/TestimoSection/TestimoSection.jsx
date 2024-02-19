eb8aa7656bad416e9c90906a526e9be1c65d2824
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-19 11:46:22 +0000

    top of TestimoSection finished

--- src/Sections/TestimoSection/TestimoSection.jsx
+++ src/Sections/TestimoSection/TestimoSection.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import './TestimoSection.css'
 const TestimoSection = () => {
   return (
-    <div className='testimo-section'>TestimoSection</div>
+    <div className='testimo-section'>
+        <div className="top">
+            <p className="section-heading">testimonial</p>
+            <p className="section-header">what are they saying</p>
+            <p className="section-text">Lorem ipsum dolor sit amet consectetur. Non tincidunt <br />magna non et elit. Dolor  turpis molestie dui <br />magnis facilisis at fringilla quam</p>
+        </div>
+        
+    </div>
   )
 }
 
