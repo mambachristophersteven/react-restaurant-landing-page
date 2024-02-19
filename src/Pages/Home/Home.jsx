247d943a710f7f39d20d554526f88f2d3c145681
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-19 09:06:32 +0000

    initializing AboutSection in Home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Home.css'
 import HomeSection from '../../Sections/HomeSection/HomeSection'
+import AboutSection from '../../Sections/AboutSection/AboutSection'
 
 
 const Home = () => {
   return (
     <div className='home'>
       <HomeSection />
+      <AboutSection />
     </div>
   )
 }
---
+++ src/Sections/AboutSection/AboutSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './AboutSection.css'
+const AboutSection = () => {
+  return (
+    <div className='about-section'>
+        <p>hello</p>
+    </div>
+  )
+}
+
+export default AboutSection
