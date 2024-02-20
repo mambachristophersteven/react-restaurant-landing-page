7214ef470078f8a2158266aaa0ed17ba6fa03af3
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-20 10:10:19 +0000

    content and images added to Footer component

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import './Footer.css'
+import youtube_icon from '../../Assets/youtube-icon.svg'
+import facebook_icon from '../../Assets/facebook-icon.svg'
+import twitter_icon from '../../Assets/twitter-icon.svg'
+import linkedin_icon from '../../Assets/linkedin-icon.svg'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className="footer-left">
+        <p className="foodie-name">FOODIE</p>
+        <div className="social-media">
+          <img src={twitter_icon} alt="" />
+          <img src={linkedin_icon} alt="" />
+          <img src={youtube_icon} alt="" />
+          <img src={facebook_icon} alt="" />
+        </div>
+      </div>
+      <div className="footer-right">
+        <div className="footer-links">
+          <Link to='#'><p className='footer-link'>quality</p></Link>
+          <Link to='#'><p className='footer-link'>help</p></Link>
+          <Link to='#'><p className='footer-link'>share</p></Link>
+          <Link to='#'><p className='footer-link'>carriers</p></Link>
+          <Link to='#'><p className='footer-link'>work</p></Link>
+          <Link to='#'><p className='footer-link'>testimonials</p></Link>
+        </div>
+        <div className="footer-links">
+          <Link to='#'><p className='footer-link'>+233 980 456 670</p></Link>
+          <Link to='#'><p className='footer-link'>hello@food.com</p></Link>
+          <Link to='#'><p className='footer-link'>press@food.com</p></Link>
+          <Link to='#'><p className='footer-link'>contact@food.com</p></Link>
+        </div>
+        <div className="footer-links">
+          <Link to='#'><p className='footer-link'>terms & conditions</p></Link>
+          <Link to='#'><p className='footer-link'>privacy policy</p></Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
