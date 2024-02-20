9193e8de1635789b602d17e1e88dc5c34c73ed5a
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-20 09:28:46 +0000

    ContactSection initialized in Home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import HomeSection from '../../Sections/HomeSection/HomeSection'
 import AboutSection from '../../Sections/AboutSection/AboutSection'
 import WorkSection from '../../Sections/WorkSection/WorkSection'
 import TestimoSection from '../../Sections/TestimoSection/TestimoSection'
+import ContactSection from '../../Sections/ContactSection/ContactSection'
 
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
       <AboutSection />
       <WorkSection />
       <TestimoSection />
+      <ContactSection />
     </div>
   )
 }
---
+++ src/Sections/ContactSection/ContactSection.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ContactSection.css'
+
+const ContactSection = () => {
+  return (
+    <div className='contact-section'>ContactSection</div>
+  )
+}
+
+export default ContactSection
