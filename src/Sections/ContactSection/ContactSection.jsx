d4a8a82366c61a656dc1c4f4c98d2e3ad40fdcad
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-20 09:35:03 +0000

    content added for ContactSection

--- src/Sections/ContactSection/ContactSection.jsx
+++ src/Sections/ContactSection/ContactSection.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './ContactSection.css'
+import Button from '../../Components/Button/Button'
+import arrow from '../../Assets/arrow.svg'
 
 const ContactSection = () => {
   return (
-    <div className='contact-section'>ContactSection</div>
+    <div className='contact-section'>
+      <div className="contact-top">
+        <p className="section-header">have question in mind?</p>
+        <p className="section-header">let us help you</p>
+      </div>
+      <div className="contact-bottom">
+        <form action="#">
+          <input type="email" name="email" id="email" placeholder='yourmail@gmail.com'/>
+          <Button content='submit' image={arrow} />
+        </form>
+      </div>
+    </div>
   )
 }
 
