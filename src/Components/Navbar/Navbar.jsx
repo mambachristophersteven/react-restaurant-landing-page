edaea61858fb78a8a688c91a9a113a6840969ba2
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-24 17:29:19 +0000

    mobile responsiveness for navbar component initialized

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,21 +5,21 @@ import cart from '../../Assets/shopping-cart.svg'
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <div className="logo">
-            <p className="logo-text">FOODIE</p>
-        </div>
-        <div className="nav-menu">
-            <ul>
-                <li>home</li>
-                <li>about</li>
-                <li>testimonials</li>
-                <li>contact</li>
-                <li>
-                  <img src={cart} alt="" />
-                </li>
-                <li><button>booking now</button></li>
-            </ul>
-        </div>
+      <div className="logo">
+        <p className="logo-text">FOODIE</p>
+      </div>
+      <div className="nav-menu">
+        <ul>
+          <li>home</li>
+          <li>about</li>
+          <li>testimonials</li>
+          <li>contact</li>
+          <li>
+            <img src={cart} alt="" />
+          </li>
+          <li><button>booking now</button></li>
+        </ul>
+      </div>
     </div>
   )
 }
