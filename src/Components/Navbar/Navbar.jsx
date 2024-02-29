13ecfdf2c971a0c604dc1ae2d65cd5348c99f74f
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-29 11:56:49 +0000

    finished popup open and close

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,9 +10,11 @@ const Navbar = () => {
 
   function openBox(){
     console.log('open');
+    setOpen(true);
   }
   function closeBox(){
     console.log('close');
+    setOpen(false);
   }
   return (
     <div className='navbar'>
@@ -34,27 +36,31 @@ const Navbar = () => {
       <div className="menu-icon">
         <img onClick={openBox} src={menu_svg} alt="" />
       </div>
-      <div className="mobile-popup">
-        <div className="mobile-top">
-          <img src={popup_image} alt="" />
-          <img onClick={closeBox} src={close_icon} alt="" />
-        </div>
-        <div className="mobile-links">
-          <ul>
-            <li>home</li>
-            <li>about</li>
-            <li>testimonials</li>
-            <li>contact</li>
-            <li>
-              <img src={cart} alt="" />
-            </li>
-            <li><button>booking now</button></li>
-          </ul>
-        </div>
-        <div className="mobile-logo">
-          <p className="mobile-logo-text">FOODIE</p>
+      {
+        open &&
+        <div className="mobile-popup">
+          <div className="mobile-top">
+            <img src={popup_image} alt="" />
+            <img onClick={closeBox} src={close_icon} alt="" />
+          </div>
+          <div className="mobile-links">
+            <ul>
+              <li>home</li>
+              <li>about</li>
+              <li>testimonials</li>
+              <li>contact</li>
+              <li>
+                <img src={cart} alt="" />
+              </li>
+              <li><button>booking now</button></li>
+            </ul>
+          </div>
+          <div className="mobile-logo">
+            <p className="mobile-logo-text">FOODIE</p>
+          </div>
       </div>
-      </div>
+      }
+      
     </div>
   )
 }
